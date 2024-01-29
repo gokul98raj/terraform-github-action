8846e27565f68852bd7b9dabb8897c4978282dcf
Gokul Raj <42057165+gokul98raj@users.noreply.github.com>
2024-01-29 11:55:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "ami-0d13e3e640877b0b9"  # Set your desired AMI
-  instance_type = "t2.micro"               # Set your desired instance type
+  instance_type = "t2.nano"               # Set your desired instance type
 
   tags = {
     Name = "example-instance"
